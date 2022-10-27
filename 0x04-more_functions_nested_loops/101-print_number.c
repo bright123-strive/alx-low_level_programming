297d9261c4b49efa5427ed0f3301b5ab3165129b
bright123-strive <brimuwemi@gmail.com>
2022-10-27 07:37:20 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -23,13 +23,3 @@ void print_number(int n)
  * @m: an input unsigned integer
  * Return: Nothing
  */
-void print_integer(int m)
-{
-	int i = 1000000000;
-
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
-}
