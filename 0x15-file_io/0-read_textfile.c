78a8b995878af09d6294a1f0ed2e4b4a022487d8
bright123-strive <brimuwemi@gmail.com>
2022-11-04 07:39:07 -0700

    one

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,47 @@
-nclude <sys/types.h>
-#include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- *  * read_textfile - A function that reads a text file and prints
- *   * to the POSIX STDOUT
- *    * @filename: The filename to open
- *     * @letters: The number of letters to read and print
- *      * Return: The number of letters read and printed, or 0 on failure
- *       */
+ * read_textfile - Entry Point
+ * @filename: file name
+ * @letters: size
+ * Return: 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-		int fdo, fdr, fdw;
-			char *temp;
+	int file, rd, wr;
+	char *buf;
 
-				if (filename == NULL)
-							return (0);
 
-					temp = malloc(sizeof(char) * letters);
-						if (temp == NULL)
-									return (0);
+	if (filename == NULL)
+		return (0);
 
-							fdo = open(filename, O_RDONLY);
-								if (fdo < 0)
-										{
-													free(temp);
-															return (0);
-																}
 
-									fdr = read(fdo, temp, letters);
-										if (fdr < 0)
-												{
-															free(temp);
-																	return (0);
-																		}
+	file = open(filename, O_RDONLY);
 
-											fdw = write(STDOUT_FILENO, temp, fdr);
-												free(temp);
-													close(fdo);
 
-														if (fdw < 0)
-																	return (0);
-															return ((ssize_t)fdw);
+	if (file == -1)
+		return (0);
+
+
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
+		return (0);
+
+
+	rd = read(file, buf, letters);
+	if (rd == -1)
+		return (0);
+
+
+	buf[letters] = '\0';
+
+
+	wr = write(1, buf, rd);
+	if (wr == -1)
+		return (0);
+
+
+	close(file);
+	free(buf);
+	return (wr);
 }
